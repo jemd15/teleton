c9d1e9b0ed35daa1896ef245ce1ba0fdf89dd327
Desck <cgonzalezordenes@gmail.com>
2016-09-09 02:38:48 -0300

    Carrusel
    Algo tiene que no muestra

--- app/js/app.js
+++ app/js/app.js
@@ -58,3 +58,15 @@ angular.module('ideaton',
 
         $urlRouterProvider.otherwise('/inicio')
     });
+
+  $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+
+var app = angular.module('materializeApp', ['ui.materialize'])
+    .controller('BodyController', ["$scope", function ($scope) {
+        $scope.select = {
+            value: "Option1",
+            choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
+        };
+    }]);

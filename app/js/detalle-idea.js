05910d4a4e41d7510c9b9815cff3f894111dbdc8
juliogf <julio_gonzalezf@hotmail.com>
2016-09-16 13:14:42 -0300

    facebook

--- app/js/detalle-idea.js
+++ app/js/detalle-idea.js
@@ -3,9 +3,21 @@
  */
 angular.module('detalle-idea',[])
 
-    .controller('IdeaCtrl', ['$scope','$location', function ($scope,$location) {
-          console.log("hola");
-        console.log("id:"+$location.search().id);
+    .controller('IdeaCtrl', ['$scope','$location','$http', function ($scope,$location,$http) {
+        var id_idea = $location.search().id;
+        $http.get('http://pyhackaton2016-hackatonteleton.rhcloud.com/ideas/'+id_idea+"/")
+            .success(function (data, status, headers, config) {
+                    $scope.nombre= data.title;
+                $scope.descripcion_corta=data.short_description;
+                $scope.descripcion= data.description;
+                $scope.url_vid = data.url_video;
+                $scope.beneficiario=data.beneficiary;
+                $scope.categoria = data.category;
+                $scope.img_prin=    data.main_image;
+            })
+            .error(function (data, status, header, config) {
+                  console.log("FALLO:"+data);
+            });
     }]);
 
 

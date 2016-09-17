e743882040b1f778e0e7ca31abaa7247d94dd7c1
juliogf <julio_gonzalezf@hotmail.com>
2016-09-16 21:25:39 -0300

    javier pls

--- app/js/detalle-idea.js
+++ app/js/detalle-idea.js
@@ -7,17 +7,38 @@ angular.module('detalle-idea',[])
         var id_idea = $location.search().id;
         $http.get('http://pyhackaton2016-hackatonteleton.rhcloud.com/ideas/'+id_idea+"/")
             .success(function (data, status, headers, config) {
-                    $scope.nombre= data.title;
+                $scope.nombre= data.title;
                 $scope.descripcion_corta=data.short_description;
                 $scope.descripcion= data.description;
                 $scope.url_vid = data.url_video;
                 $scope.beneficiario=data.beneficiary;
                 $scope.categoria = data.category;
                 $scope.img_prin=    data.main_image;
+                $scope.autor=    data.user.email;
+                $scope.comuna = data.user.commune;
+
             })
             .error(function (data, status, header, config) {
                   console.log("FALLO:"+data);
             });
+
+        $http.get('http://pyhackaton2016-hackatonteleton.rhcloud.com/imagenes/?idea__id='+id_idea)
+            .success(function (data, status, headers, config) {
+               console.log(data.results);
+                $scope.imagenes =data.results;
+
+            })
+            .error(function (data, status, header, config) {
+                console.log("FALLO:"+data);
+            });
+
+    }])
+
+
+    .controller('VotosCtrl', ['$scope','$location','$http', function ($scope,$location,$http) {
+
+
+
     }]);
 
 

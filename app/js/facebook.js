7f83ca044e7cfe1c0c1bd1551672e0d6cc02744e
juliogf <julio_gonzalezf@hotmail.com>
2016-09-22 01:31:26 -0300

    comentarios listocos

--- app/js/facebook.js
+++ app/js/facebook.js
@@ -96,6 +96,7 @@
                         $scope.user = response;
                         $sessionStorage.emailface=response.email;
                         $sessionStorage.nombre=response.name;
+                        $scope.nombre =response.name;
                         console.log($scope.user);
 
                     });
@@ -144,7 +145,7 @@
                                 $sessionStorage.tokenface = data.key;
                                 $sessionStorage.isloginface = 1;
                                 swal({
-                                        title: "Bienvenido!\n"+$sessionStorage.nombre,
+                                        title: "Bienvenido!\n"+$scope.nombre,
                                         type: "success",
                                         confirmButtonColor: "#DD6B55",
                                         confirmButtonText: "Aceptar",

c1ca91c21a13e03834a8cdd1495faccac5b211ee
juliogf <julio_gonzalezf@hotmail.com>
2016-09-25 16:49:46 -0300

    arregle contador :)

--- app/js/countdown.js
+++ app/js/countdown.js
@@ -1,3 +1,61 @@
+(function () {
+	angular.module('contador', ['ngSanitize'])
+
+		.directive('countdown', ['Util', '$interval', function (Util, $interval) {
+			return {
+				restrict: 'A',
+				scope: { date: '@' },
+				link: function (scope, element,$scope) {
+					var future;
+					future = new Date(scope.date);
+					$interval(function () {
+						var diff;
+						diff = Math.floor((future.getTime() - new Date().getTime()) / 1000);
+						var time = Util.dhms(diff);
+
+						return element.html("<span>"+time.d+"</span>"
+							+"<span>"+time.h+"</span>"
+							+"<span>"+time.m+"</span>"
+						    +"<span>"+time.s+"</span>");
+					}, 1000);
+				}
+			};
+		}
+	])
+		.factory('Util', [function () {
+		return {
+			dhms: function (t) {
+				var days, hours, minutes, seconds;
+				days = Math.floor(t / 86400);
+				t -= days * 86400;
+				hours = Math.floor(t / 3600) % 24;
+				t -= hours * 3600;
+				minutes = Math.floor(t / 60) % 60;
+				t -= minutes * 60;
+				seconds = t % 60
+				var cont2 ={
+					d:days,
+					h:hours,
+					m:minutes,
+					s:seconds,
+				}
+				var cont = [
+						days + 'd',
+						hours + 'h',
+						minutes + 'm',
+						seconds + 's'
+						].join('');
+				console.log(cont2)
+				return cont2 ;
+
+			}
+		};
+	}]);
+}.call(this));
+
+
+
+/*
 angular.module('contador',['ngSanitize'])
 
 	.controller('ContadorCtrl', ['$scope','$location','$timeout', function ($scope,$location,$timeout) {
@@ -59,8 +117,4 @@ angular.module('contador',['ngSanitize'])
 
 	}]);
 
-
-
-
-
-
+*/

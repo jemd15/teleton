fc4e4c20e28b7f871cf2fb0e8d9fe7592e610ed7
juliogf <julio_gonzalezf@hotmail.com>
2016-09-25 12:01:38 -0300

    cambios noticias

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,2 @@
-angular.module('ideaton',['rutas','AppFace', 'AppGoogle','validacion','registro','SubirIdea','detalle-idea','inicio','detalle-categoria','contador']);
+angular.module('ideaton',['rutas','AppFace', 'AppGoogle','validacion','registro','SubirIdea','detalle-idea','inicio','detalle-categoria','contador','detalle-noticia']);
 
---
+++ app/js/detalle-noticia.js
@@ -0,0 +1,20 @@
+/**
+ * Created by juliogf on 25-09-16.
+ */
+angular.module('detalle-noticia',[])
+
+.controller('NoticiaCtrl',['$scope','$location','$http','$stateParams', function ($scope,$location,$http,$stateParams) {
+
+    var id_noticia = $stateParams.noticiaID;
+    $http.get('http://pyhackaton2016-hackatonteleton.rhcloud.com/noticias/'+id_noticia+"/")
+        .success(function (data, status, headers, config) {
+            $scope.noticia= data;
+
+
+
+        })
+        .error(function (data, status, header, config) {
+            console.log("FALLO:"+data);
+        });
+
+}]);
